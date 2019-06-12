c607899af9e5c93ce614b600f1134c7c6cb8c330
Matt Beland <matt@rearviewmirror.org>
2019-06-12 15:27:42 -0700

    Cleanup print functions, move to .format; add host tags to individual output messages

--- fabfile.py
+++ fabfile.py
@@ -3,12 +3,13 @@ from os import *
 
 
 env.use_ssh_config = True
+env.output_prefix = True
 scriptFileName = None
 repoList = {}
 
 
 def get_immediate_subdirectories(a_dir='~/repos/'):
-    result = run("find %s -maxdepth 1 -type d" % a_dir)
+    result = run("find {} -maxdepth 1 -type d".format(a_dir))
     result = result.splitlines()
     del result[0]
     return result
@@ -22,7 +23,7 @@ def reboot():
         try:
             sudo("shutdown -r 1", shell=False)
         except Exception as e:
-            print(str(e))
+            print("{0} - {1}".format(env.host_string, e))
             is_done = False
         finally:
             return is_done
@@ -33,12 +34,12 @@ def apt_update():
         is_done = True
 
         try:
-            print("Updating system...")
+            print("Updating {} with apt...".format(env.host_string))
             sudo("apt update", shell=False)
             sudo("DEBIAN_FRONTEND=noninteractive apt upgrade -yq", shell=False,
                  timeout=120)
         except Exception as e:
-            print(str(e))
+            print("{0} - {1}".format(env.host_string, e))
             is_done = False
         finally:
             return is_done
@@ -49,10 +50,10 @@ def yum_update():
         is_done = True
 
         try:
-            print("Updating system...")
+            print("Updating {} with yum...".format(env.host_string))
             sudo("yum -y update", shell=False)
         except Exception as e:
-            print(str(e))
+            print("{0} - {1}".format(env.host_string, e))
             is_done = False
         finally:
             return is_done
@@ -63,16 +64,16 @@ def opkg_update():
         is_done = True
 
         try:
-            print("Updating system...")
+            print("Updating {} with opkg...".format(env.host_string))
             run("opkg update", shell=False)
             packages = run("opkg list-upgradable | awk '{print $1}' | "
                            "sed ':M;N;$!bM;s#\\n# #g'", shell=False)
             if packages:
                 run("opkg upgrade %s" % packages, shell=False)
             else:
-                print("No updated packages")
+                print("No updated packages on {}".format(env.host_string))
         except Exception as e:
-            print(str(e))
+            print("{0} - {1}".format(env.host_string, e))
             is_done = False
         finally:
             return is_done
@@ -86,18 +87,18 @@ def repo_update(repoName="Home Bin", repoDirectory="~/bin/"):
             with cd(repoDirectory):
                 pathList = run('ls -a')
                 if ".noPull" in pathList.stdout:
-                    print("%s has .noPull - skipping" % repoName)
+                    print("{} repo {} has .noPull - skipping".format(env.host_string, repoName))
                     return is_done
                 r = run('git pull')
                 if 'Already up-to-date' in r.stdout:
-                    print("%s already up-to-date" % repoName)
+                    print("{} repo {} already up-to-date".format(env.host_string, repoName))
                 else:
-                    print("%s updated" % repoName)
+                    print("{} repo {} updated".format(env.host_string, repoName))
                 if '.postpull.sh' in pathList.stdout:
-                    print("%s - updating permissions" % repoName)
+                    print("{} repo {} - updating permissions".format(env.host_string, repoName))
                     run('./.postpull.sh')
         except Exception as e:
-            print(str(e))
+            print("{0} - {1}".format(env.host_string, e))
             is_done = False
         finally:
             return is_done
@@ -106,21 +107,22 @@ def repo_update(repoName="Home Bin", repoDirectory="~/bin/"):
 def setup():
     with hide('running', 'stdout', 'stderr'):
         is_done = True
-        apt_sudoers = "matt * = (root) NOPASSWD:SETENV: /usr/bin/apt"
+        apt_sudoers = "matt * = (root) NOPASSWD:SETENV: /usr/bin/apt,"\
+            "/usr/local/bin/apt"
         power_sudoers = "matt * = (root) NOPASSWD: /sbin/shutdown"
 
         try:
-            sudo("echo \"%s\" >> /etc/sudoers.d/fabric" % apt_sudoers)
-            sudo("echo \"%s\" >> /etc/sudoers.d/fabric" % power_sudoers)
+            sudo("echo \"{}\" >> /etc/sudoers.d/fabric".format(apt_sudoers))
+            sudo("echo \"{}\" >> /etc/sudoers.d/fabric".format(power_sudoers))
             pathList = run('ls -a ~')
             if "repos" not in pathList.stdout:
-                print("No repos dir - creating")
+                print("No repos dir on {} - creating".format(env.host_string))
                 run("mkdir ~/repos/")
             if "bin" not in pathList.stdout:
-                print("No bin dir - cloning")
+                print("No bin dir on {} - cloning".format(env.host_string))
                 run("git clone https://github.com/mbeland/bin.git")
         except Exception as e:
-            print(str(e))
+            print("{0} - {1}".format(env.host_string, e))
             is_done = False
         finally:
             return is_done
@@ -135,7 +137,7 @@ def updates():
             apt_update()
             updateRepos()
         except Exception as e:
-            print(str(e))
+            print("{0} - {1}".format(env.host_string, e))
             is_done = False
         finally:
             return is_done
@@ -155,7 +157,7 @@ def updateRepos():
                 y = x.split("/")
                 repo_update(y[-1], x)
         except Exception as e:
-            print(str(e))
+            print("{0} - {1}".format(env.host_string, e))
             is_done = False
         finally:
             return is_done
@@ -174,7 +176,7 @@ def deploy_script():
             run(remotePath)
             run("rm " + remotePath)
         except Exception as e:
-            print(str(e))
+            print("{0} - {1}".format(env.host_string, e))
             is_done = False
         finally:
             return is_done
